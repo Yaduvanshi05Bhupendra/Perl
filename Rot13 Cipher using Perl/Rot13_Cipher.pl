84a9caf0d7f5c3d421e12949b220771ccfd8b513
Bhupendra Kumar Yadav <67783654+Yaduvanshi05Bhupendra@users.noreply.github.com>
2021-01-17 15:51:30 +0530

    Update Rot13_Cipher.pl

--- Rot13 Cipher using Perl/Rot13_Cipher.pl
+++ Rot13 Cipher using Perl/Rot13_Cipher.pl
@@ -1,4 +1,4 @@
-print "Enter sentence: ";
+print "Enter sentence: \n";
 chomp($sentence=<STDIN>);
 @rotarray =();
 @character = split(//,$sentence);
@@ -7,12 +7,14 @@ for ($i=0;$i<$N;$i=$i+1) {
 rot13 ($character[$i]);
 }
 $rotsen = join("",@rotarray);
-print "Rot13 of $sentence is: $rotsen";
+print "Normal Form: $sentence \nRot13_Cipher: $rotsen";
 
+#Calling subroutine to convert sentence in Rot13 Cipher
 sub rot13 {
 chomp($char=$_[0]);
 $ascii_num = ord($char);
 $rot13_num = $ascii_num + 13;
+#Conversion of capital letter
 if ($ascai_num > 64 && $ascii_num < 91) {
     if ($rot13_num>90) {
     $rot13_char = chr($rot13_num - 26);
@@ -21,6 +23,7 @@ if ($ascai_num > 64 && $ascii_num < 91) {
     $rot13_char = chr($rot13_num);
     push (@rotarray,"$rot13_char");
 }
+#Conversion of small letter
 } elsif ($ascii_num > 96 && $ascii_num < 123) {
     if ($rot13_num>122) {
     $rot13_char = chr($rot13_num - 26);
@@ -29,6 +32,7 @@ if ($ascai_num > 64 && $ascii_num < 91) {
     $rot13_char = chr($rot13_num);
     push (@rotarray,"$rot13_char");
     }
+#Anything other than alphabet will left as it is.
 } else {
      $rot13_char = chr($ascii_num);
      push (@rotarray,"$rot13_char");
